86d9c8e2257746ab2f52b6dd9a7eaf71a2017d39
Wilovy09 <srwilovy09@gmail.com>
2024-12-04 00:05:06 -0600

    :sparkles: Add things for EntityFramework

--- microservices/UserService/Program.cs
+++ microservices/UserService/Program.cs
@@ -1,14 +1,17 @@
 using UserService.Services;
+using UserModel.Models;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddGrpc();
+builder.Services.AddDbContext<UserContext>(o =>
+{
+    o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
-app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+app.MapGrpcService<ManageUserService>();
 
 app.Run();
